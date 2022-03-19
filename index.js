b110c7f20eada6513a8431f93a979f006f5b655f
Joe Scott <jbs198585@gmail.com>
2022-03-19 10:43:54 -0400

    subreddit.ejs

--- index.js
+++ index.js
@@ -6,11 +6,17 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', (req, res) => {
-  res.render('home.ejs');
+  res.render('home');
+});
+
+app.get('/r/:subreddit', (req, res) => {
+  const {subreddit} = req.params;
+  res.render('subreddit', { subreddit });
 });
 
 app.get('/rand', (req, res) => {
-  res.render('random');
+  const num = Math.floor(Math.random() * 10) + 1;
+  res.render('random', { num });
 });
 
 app.listen(3000, () => {

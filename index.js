97c5b87c917e024e4c01f75aa90b7946bb25bf55
Joe Scott <jbs198585@gmail.com>
2022-03-19 10:15:45 -0400

    random.ejs

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
+const path = require('path');
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "/views"));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
-app.get("/", (req, res) => {
-  res.render("home.ejs");
+app.get('/', (req, res) => {
+  res.render('home.ejs');
+});
+
+app.get('/rand', (req, res) => {
+  res.render('random');
 });
 
 app.listen(3000, () => {

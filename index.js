a7f832e15286d52c445581da27bcd630155f7901
Joe Scott <jbs198585@gmail.com>
2022-03-20 08:14:09 -0400

    Serving static assets in Express

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const redditData = require('./data.json');
-console.log(redditData);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
   res.render('home');
